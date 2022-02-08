497750d7c3ad97cb7b0246a06f468de4a3cd1cbe
Paulo Victor <paulo.victor.ig@hotmail.com>
2022-02-08 20:59:33 -0300

    'particles'

--- src/App.js
+++ src/App.js
@@ -12,17 +12,17 @@ import './App.css';
 
 
 
-// const particlesOptions = {
-//   particles: {
-//     number: {
-//       value: 30,
-//       density: {
-//         enable: true,
-//         value_area: 800
-//       }
-//     }
-//   }
-// }
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 const initialState = {
     input: '',
@@ -119,9 +119,9 @@ class App extends Component {
     const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
-         {/* <Particles className='particles'
+         <Particles className='particles'
           params={particlesOptions}
-        /> */}
+        />
         <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
         { route === 'home'
           ? <div>
